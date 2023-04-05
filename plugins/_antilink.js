51eb41b36932a8995aa465a4b3846cbc8ddbe506
Alkahyan <128998518+Alkahyan@users.noreply.github.com>
2023-04-05 19:59:30 +0200

    Add files via upload

--- plugins/_antilink.js
+++ plugins/_antilink.js
@@ -9,18 +9,18 @@ let bang = m.key.id
 let bot = global.db.data.settings[this.user.jid] || {}
 const isGroupLink = linkRegex.exec(m.text)
 const grupo = `https://chat.whatsapp.com`
-if (isAdmin && chat.antiLink && m.text.includes(grupo)) return m.reply('*تم تفعيل وضع منع الروابط لكن انت ادمن وواسطه لن يتم طردك*')
+if (isAdmin && chat.antiLink && m.text.includes(grupo)) return m.reply('*لقد ارسل ادمن واسطة رابط ولم يتم طرد الادمن!*')
 if (chat.antiLink && isGroupLink && !isAdmin) {
 if (isBotAdmin) {
 const linkThisGroup = `https://chat.whatsapp.com/${await this.groupInviteCode(m.chat)}`
 if (m.text.includes(linkThisGroup)) return !0
 }    
-await conn.sendButton(m.chat, `*「 منع الروابط 」*\n*اراك فيما بعد، وداعا بيبي 👋, ${await this.getName(m.sender)} 𝚁𝙾𝙼𝙿𝙸𝚂𝚃𝙴𝚂 𝙻𝙰𝚂 𝚁𝙴𝙶𝙻𝙰𝚂 𝙳𝙴𝙻 𝙶𝚁𝚄𝙿𝙾, 𝚂𝙴𝚁𝙰𝚂 𝙴𝚇𝚃𝙴𝚁𝙼𝙸𝙽𝙰𝙳𝙾...!!*${isBotAdmin ? '' : '\n\n*[❗𝐈𝐍𝐅𝐎❗] 𝙴𝙻 𝙱𝙾𝚃 𝙽𝙾 𝙴𝚂 𝙰𝙳𝙼𝙸𝙽, 𝙽𝙾 𝙿𝚄𝙴𝙳𝙴 𝙴𝚇𝚃𝙴𝚁𝙼𝙸𝙽𝙰𝚁 𝙰 𝙻𝙰𝚂 𝙿𝙴𝚁𝚂𝙾𝙽𝙰𝚂*'}`, author, ['𝙳𝙴𝚂𝙰𝙲𝚃𝙸𝚅𝙰𝚁 𝙰𝙽𝚃𝙸𝙻𝙸𝙽𝙺𝚂', '/disable antilink'], m)    
+await conn.sendButton(m.chat, `*「 منع الروابط 」*\n*تم اكتشاف رابط وتم طردك ايها الحثالة من المجموعة باي🐼🍷🖕, ${await this.getName(m.sender)}🕷️!!*${isBotAdmin ? '' : '\n\n*[❗معلومات❗] البوت مش اظمن يجب رفع البوت ادمن اولا*'}`, author, ['تم الغاء تفعيل منع الروابط'], m)    
 if (isBotAdmin && bot.restrict) {
 await conn.sendMessage(m.chat, { delete: { remoteJid: m.chat, fromMe: false, id: bang, participant: delet }})
-let responseb = await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')
+let responseb = await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove') زد
 if (responseb[0].status === "404") return   
-} else if (!bot.restrict) return m.reply('*[❗معلومات❗] مرحبا يا صاحب الروبوت لم بتم تمكين الاستفراغ لديك (#𝚎𝚗𝚊𝚋𝚕𝚎 𝚛𝚎𝚜𝚝𝚛𝚒𝚌𝚝) 𝙲𝙾𝙽𝚃𝙰𝙲𝚃𝙴 𝙲𝙾𝙽 𝙴𝙻 𝙿𝙰𝚁𝙰 𝚀𝚄𝙴 𝙻𝙾 𝙷𝙰𝙱𝙸𝙻𝙸𝚃𝙴*')
+} else if (!bot.restrict) return m.reply('*[❗معلومات❗] *لم يتم تمكين عمليات إعادة التعيين على مالك الروبوت (#𝚎𝚗𝚊𝚋𝚕𝚎 𝚛𝚎𝚜𝚝𝚛𝚒𝚌𝚝) اتصل به لتمكينه*')
 }
 return !0
 }
